ad8cd5e3fedd32b22daf8be493cd5f86eda84e78
Liam Black <liam.black@hedgehoglab.com>
2017-09-28 14:44:06 +0100

    Initial commit

---
+++ app/src/main/java/com/liamthedeveloper/scran/Home.java
@@ -0,0 +1,14 @@
+package com.liamthedeveloper.scran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+    }
+}
